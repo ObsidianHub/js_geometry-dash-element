e3e50215e6c7d7dfa96bf51d7186545ba7ae282b
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-06-02 15:44:31 +0300

    create rectangle object

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ context = document.querySelector("canvas").getContext("2d");
 
 context.canvas.height = 180;
 context.canvas.width = 320;
+
+rectangle = {
+  height: 32,
+  jumping: true,
+  width: 32,
+  x: 144, // center of the canvas
+  x_velocity: 0,
+  y: 0,
+  y_velocity: 0,
+};
